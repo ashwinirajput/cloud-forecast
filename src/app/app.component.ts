f186dc1416d2eb261a8a9dd26688dfcc9ea8ed29
Ashwini Kumar <ashwini@finxera.com>
2018-10-15 19:27:07 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import axios from 'axios';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-forecast';
+  constructor(private http: Http){
+
+  }
+  ngOnInit(){
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?q=London&appid=048c43a2f7e00f37c3b4044df2ec3128')
+    .then(function (response) {
+      console.log(response);
+    })
+  .catch(function (error) {
+    console.log(error);
+  });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { AppComponent } from './app.component';
-
+import * as axios from 'axios';
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: Http, useValue: axios}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
