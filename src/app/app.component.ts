a354aed6f48aa25273e6590f0ef9feeebd9bc84f
Ashwini Kumar <ashwini@finxera.com>
2018-10-16 02:27:22 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,115 @@
 import { Component } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import axios from 'axios';
+import * as  moment from 'moment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-forecast';
+  searchCity: string='London';
+  result:any;
+  todayDate:any= new Date();
+  countryDetails:any;
+  currentTab:string ='Today';
+  tabInfo: Array<{id:string,value:string,active: boolean}>=[
+    {id:'1',value:'Today',active: true},
+    {id:'2',value:'Week',active: false},
+    {id:'3',value:'Month',active: false}
+  ]
+  listing:Array<any>=[
+    {id:'dt_txt','label':'Time'},{id:'humidity','label':'Humidity'},{id:'pressure','label':'Pressure'}
+  ]
+  weatherInfo:Array<{"id": number,"main":string,"description":string,"icon":string}>=[];
+  
   constructor(private http: Http){
 
   }
   ngOnInit(){
-    axios.get('http://api.openweathermap.org/data/2.5/forecast?q=London&appid=048c43a2f7e00f37c3b4044df2ec3128')
-    .then(function (response) {
-      console.log(response);
-    })
-  .catch(function (error) {
-    console.log(error);
-  });
+      this.getListdetails();
+      this.getCountryDetails();
+      this.getWeatherDetails();
+
+  }
+  getCountryDetails(){
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q='+this.searchCity+'&units=metric&appid=048c43a2f7e00f37c3b4044df2ec3128')
+      .then((response)=>{
+        this.countryDetails = response.data;
+      })
+    .catch((error)=> {
+      console.log(error);
+    });
+  }
+
+  getListdetails(type:any='Today'){
+    this.result = null;
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?q='+this.searchCity+'&appid=048c43a2f7e00f37c3b4044df2ec3128')
+      .then((response)=>{
+        this.result = response
+        if(type=='Today'){
+            this.result.data.list = response.data.list.filter((row:any)=>moment(row.dt_txt).format('DD/MM/YYYY')==moment(this.todayDate).format('DD/MM/YYYY'));
+        }else if(type=='Week'){
+          let result=[];
+          for(let i=0;i<5;i++){
+              var todayDate = new Date();
+              todayDate.setDate(todayDate.getDate()+i);
+              let index = response.data.list.findIndex((row:any)=>moment(row.dt_txt).format('DD/MM/YYYY')==moment(todayDate).format('DD/MM/YYYY'));
+             if(index!=-1){
+                result.push(response.data.list[index]);
+             }
+          }
+          this.result.data.list = result;
+        }else{
+          let result=[];
+          for(let i=0;i<12;i++){
+             var todayDate = new Date();
+             todayDate.setMonth(todayDate.getMonth()+i);
+             let index = response.data.list.findIndex((row:any)=>moment(row.dt_txt).format('DD/MM/YYYY')==moment(todayDate).format('DD/MM/YYYY'));
+             if(index!=-1){
+                result.push(response.data.list[index]);
+             }
+          }
+          this.result.data.list = result;
+        }
+      })
+    .catch((error)=> {
+      console.log(error);
+    });
+  }
+
+  getWeatherDetails(){
+    axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+this.searchCity+'&units=metric&cnt=4&appid=048c43a2f7e00f37c3b4044df2ec3128')
+      .then((response)=>{
+        this.weatherInfo= response.data.list;
+        console.log(this.weatherInfo);
+      })
+     
+    .catch((error)=> {
+      console.log(error);
+    });
+  }
+  value(data:any){
+    return   Math.trunc( data );
+  }
+  getDay(index){
+    var date = new Date();
+    date.setDate(date.getDate()+index)
+    return date;
+  }
+  changeTab(row){
+      this.tabInfo.forEach((info:any)=>{
+        if(info.id!=row.id){
+          info.active = false;
+        }  
+      })
+      this.currentTab =row.value;
+      this.getListdetails(row.value);
+      return row.active = true;
+  }
+  doSearch(name:any){
+      this.getListdetails();
+      this.getCountryDetails();
+      this.getWeatherDetails();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import * as axios from 'axios';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [{provide: Http, useValue: axios}],
   bootstrap: [AppComponent]
